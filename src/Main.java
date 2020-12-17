a3f52f8b6f5b6069d7df38a701bd7fa78180cd63
LT-6FGD373\Hannes <h.roever@zertificon.com>
2020-12-17 01:30:30 +0100

    added enum class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main 
 {
@@ -10,30 +14,29 @@ public class Main
         int[] sortThis;
         int exponent = 10;
         
-        for (int i = 1024; i < 131073; i*=2) 
+        for (int i = 16384; i < 131073; i*=2) 
         {
             sortThis = new int[i];
 
             for (int j = 0; j < sortThis.length; j++) sortThis[j] = rand.nextInt(1000001);
 
-            System.out.println("\n" + "\n" + "Length of array to be sorted: " + i + " which is 2^" + exponent);
+            System.out.println("\n" + "\n" + "Length of array to be sorted: " + i + " which is 2^" + exponent++);
             System.out.println("**************************************************************************************");
-            exponent++;
-
             System.out.println("Sorting Algo                Time passed in ms           extrapolation for next run");
-
             System.out.println("**************************************************************************************" + "\n");            
             
-            System.out.print(printBenchmarkValues(runSortingFunction(1, sortThis.clone()), "Merge Sort by R. Mishra", 2));
-            System.out.print(printBenchmarkValues(runSortingFunction(2, sortThis.clone()), "Merge Sort by A. Schmietendorf", 2));
-            System.out.print(printBenchmarkValues(runSortingFunction(3, sortThis.clone()), "Insertion Sort", 4));
-            System.out.print(printBenchmarkValues(runSortingFunction(4, sortThis.clone()), "Insertion Sort optimized", 4));
-            System.out.print(printBenchmarkValues(runSortingFunction(5, sortThis.clone()), "Selection Sort", 4));
-            System.out.print(printBenchmarkValues(runSortingFunction(6, sortThis.clone()), "Selection Sort optimized", 4));
-            System.out.print(printBenchmarkValues(runSortingFunction(7, sortThis.clone()), "Bubble Sort", 4));
-            System.out.print(printBenchmarkValues(runSortingFunction(8, sortThis.clone()), "Bubble Sort by D. Fischer", 4));
-            System.out.print(printBenchmarkValues(runSortingFunction(9, sortThis.clone()), "Quick Sort", 2));
-        }   
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.QuickSort.ordinal(), sortThis.clone()), "Quick Sort", 2));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.MergeOne.ordinal(), sortThis.clone()), "Merge Sort by R. Mishra", 2));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.MergeTwo.ordinal(), sortThis.clone()), "Merge Sort by A. Schmietendorf", 2));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.JavaBuiltIn.ordinal(), sortThis.clone()), "Build in Java Collection Sort", 2));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.InsertionOptimized.ordinal(), sortThis.clone()), "Insertion Sort optimized", 4));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.Insertion.ordinal(), sortThis.clone()), "Insertion Sort", 4));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.SelectionOptimized.ordinal(), sortThis.clone()), "Selection Sort optimized", 4));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.Selection.ordinal(), sortThis.clone()), "Selection Sort", 4));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.BubbleSortTwo.ordinal(), sortThis.clone()), "Bubble Sort by D. Fischer", 4));
+            System.out.print(printBenchmarkValues(runSortingFunction(CallAlgo.BubbleSort.ordinal(), sortThis.clone()), "Bubble Sort", 4));
+
+        } 
     }
 
 
@@ -42,33 +45,37 @@ public class Main
         long startTime = System.currentTimeMillis();
         switch (functionToRun) 
         {
-            case 1:
+            case 0:
                 MergeSort.sort(sortThis, 0, sortThis.length-1);
                 break;      
-            case 2:
+            case 1:
                 MergeSort.getSplit(sortThis, 0, sortThis.length-1);
                 break;   
-            case 3:
+            case 2:
                 InsertionSortExample.insertionSort(sortThis);
                 break; 
-            case 4:
+            case 3:
                 InsertionSortExample.insertSortOptimized(sortThis);
                 break;   
-            case 5:
+            case 4:
                 SelectionSortExample.selectionSort(sortThis);
                 break;  
-            case 6:
+            case 5:
                 SelectionSortExample.minMaxSelectionSort(sortThis, sortThis.length);
                 break; 
-            case 7:
+            case 6:
                 BubbleSort.bubbleSort(sortThis);
                 break; 
-            case 8:
+            case 7:
                 BubbleSort.bubblesortAlternative(sortThis);
                 break; 
-            case 9:
+            case 8:
                 QuickSort.sort(sortThis, 0, sortThis.length-1);
                 break;    
+            case 9:
+                List<Integer> arrayAsList = Arrays.stream(sortThis).boxed().collect(Collectors.toList());
+                Collections.sort(arrayAsList);
+                break;                
             default:
                 break;
         }
@@ -82,4 +89,18 @@ public class Main
         return testedFunction + ": " + String.format(alignRight, "", benchmarkingTime + " ms           " 
         + benchmarkingTime + "*" + expectedFactor + " = " + benchmarkingTime*expectedFactor + " ms");
     }
+
+    enum CallAlgo 
+    {
+        MergeOne,
+        MergeTwo,
+        Insertion,
+        InsertionOptimized,
+        Selection,
+        SelectionOptimized,
+        BubbleSort,
+        BubbleSortTwo,
+        QuickSort,
+        JavaBuiltIn
+      }
 }
